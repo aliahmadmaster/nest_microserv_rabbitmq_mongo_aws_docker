dbdd148f9d2e3364576a64579e52299bc4830fee
aliahmadmaster <aliahmadmaster@gmail.com>
2022-07-26 11:26:20 -0700

    billing_module

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -3,10 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { OrdersController } from './orders.controller';
 import * as Joi from 'joi';
 import { OrdersService } from './orders.service';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, RmqModule } from '@app/common';
 import { OrdersRepository } from './orders.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Order, OrderSchema } from './schemas/orders.schema';
+import { BILLING_SERVICE } from './constants/service';
 
 @Module({
   imports: [
@@ -19,7 +20,10 @@ import { Order, OrderSchema } from './schemas/orders.schema';
       envFilePath: './apps/orders/.env'
     }),
     DatabaseModule,
-    MongooseModule.forFeature([{name: Order.name, schema: OrderSchema}])
+    MongooseModule.forFeature([{name: Order.name, schema: OrderSchema}]),
+    RmqModule.register({
+      name: BILLING_SERVICE
+    })
   ],
   controllers: [OrdersController],
   providers: [OrdersService, OrdersRepository],
