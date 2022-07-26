6798aabe88da21f8f4636553ac85f4ebbcf91408
aliahmadmaster <aliahmadmaster@gmail.com>
2022-07-26 04:13:22 -0700

    successfull order created with bitnami volumes

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateOrderDTO } from './dto/createOrderDTO';
 import { OrdersService } from './orders.service';
 
-@Controller()
+@Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
-  @Get()
-  getHello(): string {
-    return this.ordersService.getHello();
+  @Post()
+  async createOrder(@Body() request: CreateOrderDTO){
+    return this.ordersService.createOrder(request)
   }
 }
--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { CreateOrderDTO } from './dto/createOrderDTO';
+import { OrdersRepository } from './orders.repository';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly orderRepositor: OrdersRepository) {}
+
+  async createOrder(request: CreateOrderDTO){
+    return this.orderRepositor.create(request)
   }
 }
